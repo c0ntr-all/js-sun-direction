9434b6ffb9ec9632d02bbedcd270bee160ffc780
c0ntr-all <serg.taturin@gmail.com>
2025-08-25 00:39:59 +0300

    fix(seasons): delete useless "required" prop

--- src/App.vue
+++ src/App.vue
@@ -25,23 +25,19 @@ import AppWorkspace from "./components/AppWorkspace.vue"
 const showSun = ref(false)
 const season = defineModel('season', {
   type: String,
-  required: true,
   default: 'autumn',
   validator: (v) => ["spring", "summer", "autumn", "winter"].includes(v),
 })
 const flatWidth = defineModel('flatWidth', {
   type: Number,
-  required: true,
   default: 400
 })
 const flatHeight = defineModel('flatHeight', {
   type: Number,
-  required: true,
   default: 300
 })
 const sunCircleOffset = defineModel('sunCircleOffset', {
   type: Number,
-  required: true,
   default: 1
 })
 </script>
